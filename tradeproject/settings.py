dbc446d169524ae0fcd5d84d7ccfcd16f1f0884f
vipdeveloper47 <diffygifig@gmail.com>
2025-06-20 00:05:37 +0500

    Removed mysqlclient, using SQLite for Railway

--- tradeproject/settings.py
+++ tradeproject/settings.py
@@ -114,12 +114,13 @@ WSGI_APPLICATION = 'tradeproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = DATABASES = {
+DATABASES = DATABASES = DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
